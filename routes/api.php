4b2aff7e3c7d5a4cb0353e75cece26c5d8037c7a
Micael Ravaglia <micael.ravaglia@gmail.com>
2026-07-27 13:12:28 +0200

    Attivazione routing API
    Attivato routing API, creazione UsersController, creazione migrazione per aggiunta colonna lastname in tabella users

---
+++ routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use App\Http\Controllers\UsersController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/userlist', [UsersController::class, 'index'])->name('userlist');
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,40 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
+
+
+Route::get('/users', function () {
+    return User::all();
+});
+
+
+Route::get('users/{name?}/{email?}/{age?}',
+    static function (?string $name = '', ?string $email = '', ?int $age = 0) {
+        return "$name $email $age";
+    })
+    ->whereAlpha('name')
+    ->whereAlpha('email')
+    ->whereNumber('age');
+
+// Route::get('/users/{id}', function (int $id) {
+//     return User::where('id', $id)->first();
+// });
+
+// Route::get('users/{id}', static function (int $id) {
+//    return User::where('id', $id)->first();
+// })->where(['id' => '[0-9]+']);
+// //    //->where('id', '[0-9]+');
+// //    ->whereNumber('id');
+
+//Route model binding
+Route::get('users/{user}', static function (User $user) {
+    return $user;
+})
+    ->whereNumber('id');
+
+
+    Route::get('/', function () {
     return view('welcome');
 });
